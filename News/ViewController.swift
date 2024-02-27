47a23165ddad5d245173d71e883f4797d2647368
Emirhan Zeyrek <121854589+emirhanzeyrekk@users.noreply.github.com>
2024-02-27 16:32:29 +0300

    ViewController.swift

--- News/ViewController.swift
+++ News/ViewController.swift
@@ -1,12 +1,39 @@
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    private var newsTableViewModel : NewsTableViewModel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight  = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        GetData()
     }
-
-
+    func GetData() {
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/BTK-iOSDataSet/master/dataset.json")
+        WebService().NewsDownload(url: url!) { (news) in
+            if let news = news {
+                self.newsTableViewModel = NewsTableViewModel(newsList: news)
+                DispatchQueue.main.sync {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newsTableViewModel == nil ? 0 : self.newsTableViewModel.numberOfRowsInSection()
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsCell
+        let newsViewModel = self.newsTableViewModel.newsAtIndexPath(indexPath.row)
+        cell.titleLabel.text = newsViewModel.title
+        cell.storyLabel.text = newsViewModel.story
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
 }
 
